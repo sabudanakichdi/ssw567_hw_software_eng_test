a09ebbb0b77b3b652458a01d7179ae3f333873eb
sabudanakichdi <pchauha2@stevens.edu>
2022-09-06 00:13:55 -0400

    HW 00 - Tools Setup and Init test Functions

---
+++ HW00 Hello World Test/helloWorld.py
@@ -0,0 +1,10 @@
+def hello_func():
+    print ("Hello World. This is SSW 567 HW00 from Prateek")
+
+def init_func(value):
+    if(value > 0):
+        return 1
+    return 0
+
+def except_func():
+    raise Exception("Exception Caught")
---
+++ HW00 Hello World Test/initTest.py
@@ -0,0 +1,26 @@
+from pickle import FALSE, TRUE
+import unittest as u
+import helloWorld as h
+
+class initTest(u.TestCase):
+
+    def testHelloAsTrueCondition(self):
+        self.assertTrue(bool(h.init_func(100)))
+        print("Test sucessful for value TRUE")
+
+    def testHelloAsFalseCondition(self):
+        self.assertEqual(h.init_func(0), 0)
+        print("Test successful for value FALSE")
+
+    def testHelloCheckException(self):
+        self.assertRaises(Exception, h.except_func)
+        print("Test successful for caught Exception")
+
+    def testHelloForAssertionError(self):
+        try: self.assertEqual(h.init_func(0), 1)
+        except AssertionError:
+            print("Test successful for Assertion Error")        
+
+if __name__ == '__main__':
+    h.hello_func()
+    u.main()
