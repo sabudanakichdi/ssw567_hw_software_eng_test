4265939ffeaeca6dea7ac464028c545401925716
sabudanakichdi <pchauha2@stevens.edu>
2022-09-13 19:41:48 -0400

    HW01: Code fixes for exception

--- HW01 Testing triangle classification/polygon.py
+++ HW01 Testing triangle classification/polygon.py
@@ -6,17 +6,15 @@ class triangle():
 
     def is_triangle (self):
         is_not_number = []
-        if not isinstance(self.s1, (int, float)) and isinstance(self.s1, bool):
-            is_not_number.insert(s1)
-        if not isinstance(self.s2, (int, float)) and isinstance(self.s2, bool):
-            is_not_number.insert(s1)
-        if not isinstance(self.s3, (int, float)) and isinstance(self.s3, bool):
-            is_not_number.insert(s1)
+        if not isinstance(self.s1, (int, float)) or isinstance(self.s1, bool):
+            is_not_number.append(self.s1)
+        if not isinstance(self.s2, (int, float)) or isinstance(self.s2, bool):
+            is_not_number.append(self.s1)
+        if not isinstance(self.s3, (int, float)) or isinstance(self.s3, bool):
+            is_not_number.append(self.s1)
 
         if len(is_not_number)>0:
-            print("Sides")
             [print(s) for s in is_not_number]
-            print(" is not integer")
-            raise ValueError("All input value needs to be Integer")
+            raise ValueError()
             
 
--- HW01 Testing triangle classification/triangleClassifier.py
+++ HW01 Testing triangle classification/triangleClassifier.py
@@ -15,20 +15,25 @@ class triangleClassifier(t):
         return round(((self.s1**2) + (self.s2**2)),2) == round((self.s3**2), 2)
 
     def type_of_triangle (self):
-        self.is_triangle()
-        is_right = self.is_right_angled_triangle()
-        print ("Right Angled Triangle")
-        if self.is_equilateral():
-            print ("Equilateral Triangle")
-        elif self.is_isosceles():
-            print ("Isosceles Triangle")
-        else:
-            print ("Scalene Triangle")
+        try:
+            self.is_triangle()
+            is_right = self.is_right_angled_triangle()
+            print ("Right Angled Triangle")
+            if self.is_equilateral():
+                print ("Equilateral Triangle")
+            elif self.is_isosceles():
+                print ("Isosceles Triangle")
+            else:
+                print ("Scalene Triangle")
+
+        except ValueError:
+            print("ValueError: All input value needs to be Integer")
 
 if __name__ == '__main__':
     c = triangleClassifier(1,2,3)
     c.type_of_triangle()
     triangleClassifier(3,4,5).type_of_triangle()
+    triangleClassifier('abc',4,5).type_of_triangle()
 
 
     
