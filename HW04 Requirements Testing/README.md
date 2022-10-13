eb9194d1d76e89dee2f6af5d3800c3d386028626
sabudanakichdi <pchauha2@stevens.edu>
2022-10-12 23:01:36 -0400

    HW 5a: Mock Unit Test

--- HW04 Requirements Testing/README.md
+++ HW04 Requirements Testing/README.md
@@ -1,12 +1,5 @@
 [![sabudanakichdi](https://circleci.com/gh/sabudanakichdi/ssw567_hw_software_eng_test.svg?style=svg&circle-token=3c441fad2b414157223abaaf301b78cdf279cf8b)](https://circleci.com/gh/sabudanakichdi/ssw567_hw_software_eng_test)
 
 # ssw567_hw_software_eng_test
-- HW 00: Tool Setup and initialize Unit test case
-- HW 01: Testing triangle classification
-- HW 02a: Testing Leagacy Problem and Test cases
-- HW 02b: Continuous Integration Classify Triangle
-- HW 03: Decision Table and State Diagram
-- HW 04: Requirements Testing
 - HW 04a: Develop with the Perspective of the Tester in mind - Fetch user and repo details using GitHub API
-- HW 05: Static Code Analyzer
 - HW 05a: Isolate External Dependencies
--- HW04 Requirements Testing/testFetchUserDetailsService.py
+++ HW04 Requirements Testing/testFetchUserDetailsService.py
@@ -1,15 +1,42 @@
 import unittest as u
+import json
+from unittest.mock import Mock, patch
 from fetchUserDetailsService import fetchUserDetailsServiceImpl as user
+from githubClient import GitHubCL as repo_Call
 
 class TestFetchUserDetailsService(u.TestCase):
-    def testFetchUserRepos(self):
-        self.assertEquals(len(user().fetchUserRepos('sabudanakichdi')),9)
+    def test_fetch_user_repos(self):
+        self.assertEqual(len(user().fetchUserRepos('sabudanakichdi')),9)
         print('testFetchUserRepos successful')
 
-    def testFetchUserReposBlankValue(self):
+    def test_fetch_user_repos_blank_value(self):
         with self.assertRaises(ValueError):
             user().fetchUserRepos(' ')
         print('testFetchUserReposBlankValue successful')
+
+    #Mock fetch user api
+    @patch.object(repo_Call, "fetch_user_repo")
+    def test_fetch_user_repo_mock_api(self, mock_fetch_user_repo):
+        response_file = open('./json/response_fetch_user_repo.json')        
+        repo_call_response = json.load(response_file)
+        mock_fetch_user_repo.return_value = Mock(status_code = 200)
+        mock_fetch_user_repo.return_value = json.loads(json.dumps(repo_call_response))
+        response = repo_Call.fetch_user_repo('sabudanakichdi')
+        #print(response)
+        self.assertEqual(response[0]['name'],"chatbot_ner")
+        response_file.close()
+
+    #Mock fetch commit api
+    @patch.object(repo_Call, "fetch_repo_commits")
+    def test_fetch_repo_commits_mock_api(self, mock_fetch_user_repo):
+        response_file = open('./json/response_fetch_repo_commits.json')        
+        repo_call_response = json.load(response_file)
+        mock_fetch_user_repo.return_value = Mock(status_code = 200)
+        mock_fetch_user_repo.return_value = json.loads(json.dumps(repo_call_response))
+        response = repo_Call.fetch_repo_commits('sabudanakichdi', ['ssw567_hw_software_eng_test'])
+        #print(response)
+        self.assertEqual(len(response),30)
+        response_file.close()
 
 if __name__ == "__main__":
     u.main()
